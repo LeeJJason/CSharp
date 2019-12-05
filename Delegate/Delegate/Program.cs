46457c329cccda330a500b00675bbf0232558714
DESKTOP-J1F9HHH\muzi <523348613@qq.com>
2019-12-05 22:35:57 +0800

    Delegate

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+
+        delegate void Feedback(int value);
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
